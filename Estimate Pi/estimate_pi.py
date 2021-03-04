20ac75f8806e99a75f047c080327af4fcf1c173a
osorionicolas <osorionicolas95@gmail.com>
2021-03-04 03:29:36 -0300

    update estimate pi

--- Estimate Pi/estimate_pi.py
+++ Estimate Pi/estimate_pi.py
@@ -9,18 +9,21 @@
 #R = 1 
 #Pi = 4 . (cant de ptos en el circulo) / (cant total de ptos)
 
-
 import random
+import sys
 
 def estimate_pi(n):
-	num_point_circle = 0
-	num_point_total = 0
-	for _ in range(n):
-		x = random.uniform(0,1)
-		y = random.uniform(0,1)
-		distance = x**2 + y **2
-		if distance <= 1:
-			num_point_circle += 1
-		num_point_total += 1
-	
-return 4 = num_point_circle/num_point_total
+    num_point_circle = 0
+    num_point_total = 0
+    for _ in range(n):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        distance = x**2 + y **2
+        if distance <= 1:
+            num_point_circle += 1
+        num_point_total += 1
+        
+    return 4 * num_point_circle/num_point_total
+
+number = int(sys.argv[1])
+print(estimate_pi(number))
