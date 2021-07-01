2f5b27c7631ec28de8542a192324359528109563
Nicolas <osorionicolas95@gmail.com>
2021-07-01 04:12:09 -0300

    Added Floyds tortoise and hare algorithm

---
+++ Floyds Tortoise And Hare/tortoiseAndHare.py
@@ -0,0 +1,20 @@
+def findDuplicate(nums):
+	tortoise = nums[0]
+	hare = nums[0]
+	while True:
+		print(tortoise, hare)
+		tortoise = nums[tortoise]
+		hare = nums[nums[hare]]
+		if tortoise == hare:
+			print(tortoise, hare)
+			break
+			
+	ptr1 = nums[0]
+	ptr2 = tortoise
+	while ptr1 != ptr2:
+		ptr1 = nums[ptr1]
+		ptr2 = nums[ptr2]
+		
+	return ptr1
+	
+print(findDuplicate([1,4,3,4,2]))
